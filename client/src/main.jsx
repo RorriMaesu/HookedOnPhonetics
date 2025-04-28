712e4857f284df315c89595112c76f82962e5d9a
Andrew Green <andrew.green.contact@gmail.com>
2025-04-28 10:53:15 -0700

    Update BrowserRouter to use correct basename for GitHub Pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import './config/firebase'; // Initialize Firebase
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/HookedOnPhonetics">
       <App />
     </BrowserRouter>
   </React.StrictMode>
