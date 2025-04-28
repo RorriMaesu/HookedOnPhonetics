b36855fada71a31e50233840a1440381c4f2ea5d
Andrew Green <andrew.green.contact@gmail.com>
2025-04-28 10:39:52 -0700

    Add SVG icons for PWA and build client application

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.svg', 'mask-icon.svg'],
       manifest: {
         name: 'Hooked On Phonetics',
         short_name: 'HookedOnPhonetics',
@@ -18,20 +18,25 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'pwa-192x192.svg',
             sizes: '192x192',
-            type: 'image/png',
+            type: 'image/svg+xml',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'pwa-512x512.svg',
             sizes: '512x512',
-            type: 'image/png',
+            type: 'image/svg+xml',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'pwa-512x512.svg',
             sizes: '512x512',
-            type: 'image/png',
+            type: 'image/svg+xml',
             purpose: 'any maskable',
+          },
+          {
+            src: 'images/logo.png',
+            sizes: '1024x1024',
+            type: 'image/png',
           },
         ],
       },
